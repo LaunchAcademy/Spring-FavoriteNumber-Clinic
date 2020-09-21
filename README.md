f02bad51d6e7635c9be8857f1760a3ed128ecd13
prophetsfall <patrickawhelan@gmail.com>
2020-09-21 17:21:40 -0400

    final A3 code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Spring-FavoriteNumber-Clinic
+
+## Purpose
+
+Process a request and route to the appropriate template using a Spring Controller
+
+
+- create a simple `FavoriteNumber` class with a private int for `number`
+- create getters and setters
--- src/main/java/com/example/javaspringcontrollerclinic/controllers/FavoriteNumbersController.java
+++ src/main/java/com/example/javaspringcontrollerclinic/controllers/FavoriteNumbersController.java
@@ -6,29 +6,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class FavoriteNumberController {
-  @GetMapping("/")
+@RequestMapping("/favorite-numbers")
+public class FavoriteNumbersController {
+  @GetMapping
   public String getRoot(){
     return "favoriteNumbers/index";
   }
-
-  @GetMapping("/favorite-number")
-  public String getFavoriteNumber(@RequestParam Integer number, Model model) {
+  @GetMapping("/favorite")
+  public String getFavoriteNumber(@RequestParam Integer number, Model model){
     model.addAttribute("number", number);
     return "favoriteNumbers/show";
   }
-
-  @GetMapping("/favorite-number/{number}")
+  @GetMapping("/favorite/{number}")
   public String getFavoriteNumberWithPath(@PathVariable Integer number, Model model){
     model.addAttribute("number", number);
     return "favoriteNumbers/show";
   }
-
-  @GetMapping("favorite-number-bean/{number}")
-  public String getFavoriteNumberBean(@PathVariable Integer number, @ModelAttribute FavoriteNumber favoriteNumber){
+  
+  @GetMapping("/number-with-bean/{number}")
+  public String getFavoriteWithBean(@PathVariable Integer number, @ModelAttribute FavoriteNumber favoriteNumber){
     return "favoriteNumbers/show";
   }
 }
